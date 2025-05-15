2c9a74c562f91521ba528100c423973a976adc84
lingv <lingv@LUCAS>
2025-05-15 10:29:29 +0200

    Booking class 2.0

--- LibraryDV/LibraryDV/Models/Booking.cs
+++ LibraryDV/LibraryDV/Models/Booking.cs
@@ -8,13 +8,30 @@ namespace LibraryDV.Models
 {
     public class Booking
     {
+        static int _tempID = 1;
         public int UserID { get; set; }
-        public int AnimalID { get; set; }
+        public required int AnimalID { get; set; }
         public DateOnly Date { get; set; }
         public int Hour { get; set; }
         public int BookingID { get; set; }
         public string Comment { get; set; }
 
 
+
+
+
+
+        public Booking(int userID, int animalID, DateOnly date, int hour, int bookingID, string comment)
+        {
+            UserID = userID;
+            AnimalID = animalID;
+            Date = date;
+            Hour = hour;
+            BookingID = _tempID;
+            _tempID++;
+            Comment = comment;
+        }
     }
 }
+    
+
