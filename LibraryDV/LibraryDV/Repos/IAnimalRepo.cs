76979d399d0a23e16c4e2ac3d8635bfafe2ce14f
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-05-15 12:24:35 +0200

    added repo + service for animal

---
+++ LibraryDV/LibraryDV/Repos/IAnimalRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryDV.Models;
+
+namespace LibraryDV.Repos
+{
+    interface IAnimalRepo
+    {
+        //find a specific animal by ID
+        Animal GetAnimal();
+
+        //find all animals
+        List<Animal> GetAllAnimals();
+
+        //add a new animal object to the list
+        void CreateAnimal(Animal animal);
+
+        //find a specific animal by ID and remove it
+        void DeleteAnimal(int animalID);
+
+        //takes a specific animal object and returns it again after edits have been made
+        Animal EditAnimal(Animal animal);
+    }
+}
