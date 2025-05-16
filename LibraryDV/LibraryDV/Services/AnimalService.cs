2e8910aaae14038795b03c13c52108a325d925a2
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-05-16 12:30:34 +0200

    animalrepo/service

--- LibraryDV/LibraryDV/Services/AnimalService.cs
+++ LibraryDV/LibraryDV/Services/AnimalService.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryDV.Repos;
+using LibraryDV.Models;
 
 namespace LibraryDV.Services
 {
     class AnimalService
     {
-        
+        private IAnimalRepo _animalRepo;
+
+        public AnimalService(IAnimalRepo animalRepo)
+        {
+            _animalRepo = animalRepo;
+        }
+
+        public void CreateAnimal(Animal animal)
+        {
+            _animalRepo.CreateAnimal(animal);
+        }
+
+        public List<Animal> GetAllAnimals()
+        { 
+            return _animalRepo.GetAllAnimals();
+        }
+
+        //Null is a placeholder return value
+        public Animal EditAnimal()
+        {
+            return null;
+        }
+
     }
 }
