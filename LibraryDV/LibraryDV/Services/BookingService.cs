65775fa0f30c6f5a738f84ff0d67a580fd3ffade
lingv <lingv@LUCAS>
2025-05-16 11:28:26 +0200

    Booking Service

--- LibraryDV/LibraryDV/Services/BookingService.cs
+++ LibraryDV/LibraryDV/Services/BookingService.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryDV.Models;
+using LibraryDV.Repos;
 
 namespace LibraryDV.Services
 {
     class BookingService
     {
+        private IBookingRepo _bookingInterface;
+        
+        public BookingService(IBookingRepo bookRepo)
+        {
+            _bookingInterface = bookRepo ?? throw new ArgumentNullException(nameof(bookRepo));
+        }
+
+        public List<Booking> GetAllBookings()
+        {
+            return _bookingInterface.GetAllBookings();
+        }
+
+        public void CreateBooking(Booking book)
+        {
+            _bookingInterface.CreateBooking(book);
+        }
+
+        
     }
 }
